84147948faed2def846c96db575a354a0ad14da9
hoki0713 <hojeong0713@gmail.com>
2020-07-23 23:19:29 +0900

    Make the logout and mypage link and connect to the appropriate page

--- src/boxes/common/CommonHeader.tsx
+++ src/boxes/common/CommonHeader.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import { Logo, SearchBar } from '../../items';
 import './CommonPage.css'
 
 
 const CommonHeader = () => {
+  const [loginedAccount, setLoginedAccount] = useState(false);
+
   const history = useHistory();
   const handleSearch = (searchWord) => {
     alert(searchWord);
@@ -26,19 +28,35 @@ const CommonHeader = () => {
         </div>
 
         <div className="col-lg-2">
-          <div id="common-header-links">
-            <Link to="/account/login">
-              <span className="btn-link btn-sm">로그인</span>
-            </Link>
-            <Link to="/account/term-n-condition">
-              <span className="btn-link btn-sm">회원가입</span>
-            </Link>
-          </div>
+
+          {!loginedAccount &&
+            <div id="common-header-links">
+              <button onClick={() => setLoginedAccount(!loginedAccount)}>임시버튼</button>
+              <Link to="/account/login">
+                <span className="btn-link btn-sm">로그인</span>
+              </Link>
+              <Link to="/account/term-n-condition">
+                <span className="btn-link btn-sm">회원가입</span>
+              </Link>
+            </div>
+          }
+
+          {loginedAccount &&
+            <div id="common-header-links">
+              <button onClick={() => setLoginedAccount(!loginedAccount)}>임시버튼</button>
+              <Link to="/account/login">
+                <span className="btn-link btn-sm">로그아웃</span>
+              </Link>
+              <Link to="/mypage">
+                <span className="btn-link btn-sm">마이페이지</span>
+              </Link>
+            </div>
+          }
 
         </div>
 
       </div>
-    </div>
+    </div >
   );
 }
 
