d9df41b15e3f73797b1fa68829a5c521e8744e70
hoki0713 <hojeong0713@gmail.com>
2020-07-30 23:57:46 +0900

    Check several chart js features

--- src/boxes/statistic/statistic-board/StatUser.js
+++ src/boxes/statistic/statistic-board/StatUser.js
@@ -1,7 +1,9 @@
 import React from "react";
-import { Doughnut } from "react-chartjs-2";
+import { Doughnut, Bar, HorizontalBar, Line, Pie, Polar, Radar } from "react-chartjs-2";
 import "./StatUser.css";
 import useRegionStatChartData from '../statistic-hooks/useRegionStatChartData'
+
+//https://github.com/jerairrest/react-chartjs-2 참고
 
 const StatUser = () => {
 
@@ -12,7 +14,24 @@ const StatUser = () => {
       <div id="doughnutChart">
         <Doughnut data={chartData} />
       </div>
-      
+      <div>
+        <Bar data={chartData}/>
+      </div>
+      <div>
+        <HorizontalBar data={chartData}/>
+      </div>
+      <div>
+        <Line data={chartData}/>
+      </div>
+      <div>
+        <Pie data={chartData}/>
+      </div>
+      <div>
+        <Polar data={chartData}/>
+      </div>
+      <div>
+        <Radar data={chartData}/>
+      </div>
     </>
   );
 };
