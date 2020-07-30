572144e3aba36268c5c1b81397d7953bfcbbc323
hoki0713 <hojeong0713@gmail.com>
2020-07-30 23:46:43 +0900

    Make useRegionStatChartData hook for using several times

--- src/boxes/statistic/statistic-board/StatUser.js
+++ src/boxes/statistic/statistic-board/StatUser.js
@@ -1,60 +1,22 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Doughnut } from "react-chartjs-2";
 import "./StatUser.css";
-import axios from "axios";
+import useRegionStatChartData from '../statistic-hooks/useRegionStatChartData'
 
 const StatUser = () => {
-  const [keyArr, setKeyArr] = useState([]);
-  const [valueArr, setValueArr] = useState([]);
-  const [chartData, setChartData] = useState({});
 
-  const getRandomColor = () => {
-    const letters = "0123456789ABCDEF";
-    let color = "#";
-    for (let i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-  };
-
-  useEffect(() => {
-    axios
-      .get(`http://localhost:8080/statistics`)
-      .then((response) => {
-        const values = [];
-        const keys = [];
-        Object.entries(response.data).forEach(([key, value]) => {
-          keys.push(key);
-          values.push(value);
-        });
-        setKeyArr(keys);
-        setValueArr(values);
-      })
-      .catch((error) => {
-        throw error;
-      });
-  }, []);
-
-  useEffect(() => {
-    setChartData({
-      labels: keyArr,
-      datasets: [
-        {
-          data: valueArr,
-          backgroundColor: keyArr.map((key) => getRandomColor()),
-          hoverBackgroundColor: ["#FF6384", "#36A2EB", "#FFCE56"],
-        },
-      ],
-    });
-  }, [keyArr, valueArr]);
+  const chartData = useRegionStatChartData();
 
   return (
     <>
       <div id="doughnutChart">
         <Doughnut data={chartData} />
       </div>
+      
     </>
   );
 };
 
 export default StatUser;
+
+
