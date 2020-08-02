27f940f55d5e15b968756ace11cfd12b7c40918e
silver j <janggoon235@gmail.com>
2020-08-02 15:00:49 +0900

    modal test

--- src/boxes/common/board/map/Modals.tsx
+++ src/boxes/common/board/map/Modals.tsx
@@ -1,74 +1,88 @@
 import React, {useState} from "react";
-import {Button, Col, Container, Dropdown, Modal, Row} from "react-bootstrap";
+import {Button, Col, Container, Modal, Row} from "react-bootstrap";
+import {Link} from "react-router-dom";
 
 export const MapModal=(props)=> {
     const [storeLoca]=useState('경상남도 창원시 마산회원구 양덕동 157-17')
     const [storePhone]=useState('055-233-4325')
     const [reportShow, setReportShow]=useState(false)
+    const [reviewShow, setReviewShow]=useState(false)
+    const [starShow, setStarShow]=useState(false)
     const iconsize=25
     return (
         <>
-        <Modal {...props} aria-labelledby="contained-modal-title-vcenter">
-            <Modal.Header closeButton>
-                <Modal.Title id="contained-modal-title-vcenter">
-                    밀양순대국밥
-                </Modal.Title>
-            </Modal.Header>
-            <Modal.Body className="show-grid">
-                <Container>
-                    <Row>
-                        <Col xs={12} md={8}>
-                            {storeLoca}<br/>
-                            {storePhone}
-                        </Col>
-                        <Col xs={6} md={4}>
-                            <img src='http://bdap.postech.ac.kr/UPLOAD//GWPFile_per_BoardNo/80/20161114132407014640.bmp'
-                                 alt={'국밥집'} width={50} height={50}/>
+            <Modal {...props} aria-labelledby="contained-modal-title-vcenter">
+                <Modal.Header closeButton>
+                    <Modal.Title id="contained-modal-title-vcenter">
+                        밀양순대국밥
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body className="show-grid">
+                    <Container>
+                        <Row>
+                            <Col xs={12} md={8}>
+                                {storeLoca}<br/>
+                                {storePhone}
+                            </Col>
+                            <Col xs={6} md={4}>
+                                <img src='http://bdap.postech.ac.kr/UPLOAD//GWPFile_per_BoardNo/80/20161114132407014640.bmp'
+                                     alt={'국밥집'} width={50} height={50}/>
+                            </Col>
+                        </Row>
 
+                        <Row>
+                            <Col xs={6} md={4}>
+                                탕,국,찌개
+                            </Col>
+                            <Col xs={6} md={4}>
+                                별점 <img src={'https://media.istockphoto.com/vectors/five-stars-rating-vector-id1152705981'}
+                                        width={50} height={30}/>
 
-                        </Col>
-                    </Row>
+                            </Col>
+                            <Col xs={6} md={4}>
+                                {true
+                                    // sessionStorage.getItem('user')
+                                    ?
+                                    <table>
+                                        <tr><td> <img src={"https://i.pinimg.com/474x/57/62/24/5762245c37514d61a333d1d5d1434670.jpg"} width={iconsize} height={iconsize}
+                                                      onClick={()=>{setReportShow(true)}}
+                                        />&nbsp;신고하기</td></tr>
+                                        <tr><td><img src={'https://w7.pngwing.com/pngs/380/478/png-transparent-star-shape-computer-icons-stars-angle-triangle-orange-thumbnail.png'} width={iconsize} height={iconsize}
+                                                     onClick={()=>{setStarShow(true)}}
+                                        />&nbsp;즐겨찾기</td></tr>
+                                        <tr><td><img src={'https://image.flaticon.com/icons/svg/259/259500.svg'} width={iconsize} height={iconsize}
+                                                     onClick={()=>{setReviewShow(true)}}
+                                        />&nbsp;리뷰</td></tr>
+                                    </table>:
+                                    <Link to={'/account/login'}>
+                                        <table>
+                                            <tr><td>
+                                                <img src={"https://i.pinimg.com/474x/57/62/24/5762245c37514d61a333d1d5d1434670.jpg"} width={iconsize} height={iconsize}
+                                                />&nbsp;신고하기</td></tr>
+                                            <tr><td><img src={'https://w7.pngwing.com/pngs/380/478/png-transparent-star-shape-computer-icons-stars-angle-triangle-orange-thumbnail.png'} width={iconsize} height={iconsize}
+                                            />&nbsp;즐겨찾기</td></tr>
+                                            <tr><td><img src={'https://image.flaticon.com/icons/svg/259/259500.svg'} width={iconsize} height={iconsize}
+                                            />&nbsp;리뷰</td></tr>
 
-                    <Row>
-                        <Col xs={6} md={4}>
-                            탕,국,찌개
-                        </Col>
-                        <Col xs={6} md={4}>
-                            별점 <img src={'https://media.istockphoto.com/vectors/five-stars-rating-vector-id1152705981'}
-                                    width={50} height={30}/>
-
-                        </Col>
-                        <Col xs={6} md={4}>
-                            <table>
-                                <tr><td> <img src={"https://i.pinimg.com/474x/57/62/24/5762245c37514d61a333d1d5d1434670.jpg"} width={iconsize} height={iconsize}
-                                onClick={()=>{setReportShow(true)}}
-                                />신고하기</td></tr>
-                                <tr><td><img src={'https://w7.pngwing.com/pngs/380/478/png-transparent-star-shape-computer-icons-stars-angle-triangle-orange-thumbnail.png'} width={iconsize} height={iconsize}
-                                />&nbsp;즐겨찾기</td></tr>
-                                <tr><td><img src={'https://image.flaticon.com/icons/svg/259/259500.svg'} width={iconsize} height={iconsize}
-                                />&nbsp;리뷰</td></tr>
-
-                            </table>
-
-
-
-
-                        </Col>
-                    </Row>
-                </Container>
-            </Modal.Body>
-            <Modal.Footer>
-                <Button onClick={props.onHide}>Close</Button>
-            </Modal.Footer>
-        </Modal>
+                                        </table>
+                                    </Link>
+                                }
+                            </Col>
+                        </Row>
+                    </Container>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button onClick={props.onHide}>Close</Button>
+                </Modal.Footer>
+            </Modal>
             <StoreReport show={reportShow} onHide={()=>setReportShow(false)}/>
-            </>
+            <Review show={reviewShow} onHide={()=>setReviewShow(false)}/>
+            <Star show={starShow} onHide={()=>setStarShow(false)}/>
+        </>
     );
 }
 
 export const StoreReport=(props)=> {
-    const [reason,setReason]=useState('신고사유')
-    const [input,setInput]=useState(false)
     return (
         <div>
 
@@ -79,24 +93,8 @@ export const StoreReport=(props)=> {
                 <Modal.Body>
                     <img src={"https://i.pinimg.com/474x/57/62/24/5762245c37514d61a333d1d5d1434670.jpg"} width={40} height={40}
                     />
-                    &nbsp;해당 가맹점에서 지역화폐를 받지 않는다고 신고하시겠습니까?
+                    &nbsp;해당 가맹점에서 지역화폐를 받지 않습니까?
                 </Modal.Body>
-                {/*<Modal.Body>*/}
-                {/*    <Dropdown>*/}
-                {/*        <Dropdown.Toggle variant="success" id="dropdown-basic">*/}
-                {/*            {reason}*/}
-                {/*        </Dropdown.Toggle>*/}
-
-                {/*        <Dropdown.Menu>*/}
-                {/*            <Dropdown.Item onClick={()=>setReason('지역화폐 미가맹')}>지역화폐 미가맹</Dropdown.Item>*/}
-                {/*            <Dropdown.Item onClick={()=>setReason('결제시 웃돈 요구')}>결제시 웃돈 요구</Dropdown.Item>*/}
-                {/*            <Dropdown.Item onClick={()=>setReason('폐업 및 이전')}>폐업 및 이전</Dropdown.Item>*/}
-                {/*            <Dropdown.Item onClick={()=>{setReason('기타');setInput(true)}}>기타(직접 입력해주세요)</Dropdown.Item>*/}
-                {/*        </Dropdown.Menu>*/}
-                {/*    </Dropdown>*/}
-                {/*    {input&&<><h5>신고이유를 적어주세요</h5><textarea name="" id="" cols={30} rows={10}></textarea></>}*/}
-                {/*</Modal.Body>*/}
-
                 <Modal.Footer>
                     <Button variant="secondary" onClick={props.onHide}>취소</Button>
                     <Button variant="danger" onClick={props.onHide}>신고하기</Button>
@@ -109,16 +107,41 @@ export const StoreReport=(props)=> {
 };
 
 
-export const Review = () => {
+export const Review = (props) => {
     return (
         <div>
-            <Modal>
+            <Modal {...props}>
                 <Modal.Header closeButton></Modal.Header>
                 <Modal.Title>리뷰남기기</Modal.Title>
-                <Modal.Body><textarea></textarea></Modal.Body>
-                <Modal.Footer></Modal.Footer>
+                <Modal.Body><textarea name="" id="" cols={30} rows={10}></textarea></Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={props.onHide}>취소</Button>
+                    <Button variant="outline-primary" onClick={props.onHide}>리뷰남기기</Button>
+                </Modal.Footer>
             </Modal>
         </div>
     );
 };
 
+export const Star =(props)=> {
+    const [store,setStore]=useState('밀양순대국밥')
+    return (
+        <div>
+
+            <Modal {...props}>
+                <Modal.Header closeButton>
+                    <Modal.Title>가맹점 즐겨찾기 추가하기</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    {store}
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={props.onHide}>취소</Button>
+                    <Button variant="danger" onClick={props.onHide}>추가하기</Button>
+                </Modal.Footer>
+            </Modal>
+
+        </div>
+
+    );
+};
