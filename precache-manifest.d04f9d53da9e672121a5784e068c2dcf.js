5cf00658bea30be1a8bd77361310380340155dc0
hoki0713 <hojeong0713@gmail.com>
2020-08-21 22:39:10 +0900

    Updates

---
+++ precache-manifest.d04f9d53da9e672121a5784e068c2dcf.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d470937cdf0b22a7f0e58aa161f4d16",
+    "url": "/TeamProject_client/index.html"
+  },
+  {
+    "revision": "cd1ef63697ce1cd65968",
+    "url": "/TeamProject_client/static/css/2.c5f3287d.chunk.css"
+  },
+  {
+    "revision": "6c339ecf57f50dcc3734",
+    "url": "/TeamProject_client/static/css/main.eaa31f55.chunk.css"
+  },
+  {
+    "revision": "cd1ef63697ce1cd65968",
+    "url": "/TeamProject_client/static/js/2.fea33190.chunk.js"
+  },
+  {
+    "revision": "ed28c870ce478fdea750a7c5a9a7d66e",
+    "url": "/TeamProject_client/static/js/2.fea33190.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c339ecf57f50dcc3734",
+    "url": "/TeamProject_client/static/js/main.bcb7b29c.chunk.js"
+  },
+  {
+    "revision": "276f4f5a0341e5a4a510",
+    "url": "/TeamProject_client/static/js/runtime-main.2b380f52.js"
+  },
+  {
+    "revision": "93ee59f6dfd2bc7a31e5e414e1df67ca",
+    "url": "/TeamProject_client/static/media/addr.93ee59f6.png"
+  },
+  {
+    "revision": "025440721494973172dc9ccfb1e91bba",
+    "url": "/TeamProject_client/static/media/arrowMarker.02544072.png"
+  },
+  {
+    "revision": "898c434c8df7dddd1240e539c705af4f",
+    "url": "/TeamProject_client/static/media/bobIcon.898c434c.png"
+  },
+  {
+    "revision": "574e6d47d0f3766155ff638a9e972813",
+    "url": "/TeamProject_client/static/media/cafe.574e6d47.png"
+  },
+  {
+    "revision": "c9e35c107b4687c6a6fb782ca3b9d411",
+    "url": "/TeamProject_client/static/media/chinaFoodIcon.c9e35c10.png"
+  },
+  {
+    "revision": "3f74ab871398c5fe7e46af48f0e19b99",
+    "url": "/TeamProject_client/static/media/conviStore.3f74ab87.png"
+  },
+  {
+    "revision": "6546a96439cdb896a667dc57729be58a",
+    "url": "/TeamProject_client/static/media/drug.6546a964.png"
+  },
+  {
+    "revision": "6f4430c6e986ebfbc65e000cfe969c23",
+    "url": "/TeamProject_client/static/media/favStar.6f4430c6.png"
+  },
+  {
+    "revision": "c720449b53cf1903586f6dd30e9549e1",
+    "url": "/TeamProject_client/static/media/home.c720449b.png"
+  },
+  {
+    "revision": "a4ddf6ef969e4d078dd579e3f317f894",
+    "url": "/TeamProject_client/static/media/homeIcon.a4ddf6ef.png"
+  },
+  {
+    "revision": "d254523cd05f5a69c29d89ce466ad295",
+    "url": "/TeamProject_client/static/media/homeIcon2.d254523c.png"
+  },
+  {
+    "revision": "8545590228ba4f822e3119c92420b705",
+    "url": "/TeamProject_client/static/media/hospIcon.85455902.png"
+  },
+  {
+    "revision": "2d1d6c3b18786c18c16ac5008349c863",
+    "url": "/TeamProject_client/static/media/hotelIcon.2d1d6c3b.png"
+  },
+  {
+    "revision": "67930193bc71090781aa86a61ab34c9b",
+    "url": "/TeamProject_client/static/media/normal.67930193.png"
+  },
+  {
+    "revision": "677246e89122ef65f3ecfc877a430e49",
+    "url": "/TeamProject_client/static/media/phoneB.677246e8.png"
+  },
+  {
+    "revision": "b866421b14ae39365b1b452bb7356cf9",
+    "url": "/TeamProject_client/static/media/recoInfo.b866421b.png"
+  },
+  {
+    "revision": "52cc0c63bba241fdb1a0eb72e9f54eda",
+    "url": "/TeamProject_client/static/media/recom.52cc0c63.png"
+  },
+  {
+    "revision": "ad2784db5f0546da2937327b1049e9e8",
+    "url": "/TeamProject_client/static/media/red.ad2784db.png"
+  },
+  {
+    "revision": "3f46ec711099b578e74ef785cf7d7441",
+    "url": "/TeamProject_client/static/media/review.3f46ec71.png"
+  },
+  {
+    "revision": "c2145a78b40b0cfb32526de234cd621c",
+    "url": "/TeamProject_client/static/media/selected.c2145a78.png"
+  },
+  {
+    "revision": "eb9536a053468f1cbfab8e33ff25a82a",
+    "url": "/TeamProject_client/static/media/starIcon.eb9536a0.png"
+  },
+  {
+    "revision": "f4ef6208072af4463d7bf3623b74892c",
+    "url": "/TeamProject_client/static/media/store.f4ef6208.png"
+  }
+]);
