ef4059d084bd60b7ecbbdd5f6c0f7413c493b9b1
silver j <janggoon235@gmail.com>
2020-08-24 03:17:49 +0900

    sorted

--- src/boxes/common/board/map/MerchantList.js
+++ src/boxes/common/board/map/MerchantList.js
@@ -8,11 +8,7 @@ import {Link} from "react-router-dom";
 
 const MerchantList=()=> {
     const {setStore}=useContext(StoreSearchContext);
-    const [state,setState]=useState('시/군');
-    const [cate,setCate]=useState('업종');
     const [storeList, setStoreList]=useState([]);
-    const [drop1Show, setDrop1Show] = useState(false);
-    const [drop2Show, setDrop2Show] = useState(false);
     let [pageNow,setPageNow]=useState(0);
     const [totalCount,setTotalCount]=useState(0);
     const [pageSize]=useState(10);
@@ -30,28 +26,7 @@ const MerchantList=()=> {
     const [nextBlock,setNextBlock]=useState(0);
     const [pageList, setPageList]=useState([]);
     const [pageNumArr,setPageNumArr]=useState([]);
-    const [checkPageNation,setCheckPageNation]=useState(false);
 
-    const [stateList]=useState(
-        ['연천군', '포천시', '파주시', '동두천시', '양주시', '의정부시', '가평군', '고양시',
-            '김포시', '남양주시', '구리시', '하남시', '양평군', '광주시', '여주시', '이천시', '용인시', '안성시',
-            '평택시', '화성시', '수원시', '오산시', '안산시', '군포시', '의왕시', '안양시', '과천시', '부천시',
-            '광명시', '성남시', '시흥시' ]);
-    const [cateList]=useState(
-        ['숙박업', '여행', '레져용품','가구','건강식품','건축자재','광학제품', '기타','기타의료기관', '농업',
-            '레저업소', '문화.취미','병원', '보건위생', '사무통신','서적문구','수리서비스','숙박업','신변잡화','약국','용역 서비스',
-            '유통업 영리','음료식품' ]);
-    const toggle1 = () => setDrop1Show(prevState => !prevState);
-    const toggle2 = () => setDrop2Show(prevState => !prevState);
-
-    const stateCheck=(stateName)=>{
-        setState(stateName);
-        // getSpecificS();
-    };
-    const cateCheck=(category)=>{
-        setCate(category);
-        // getSpecificS();
-    };
 
     function pageNation(){
         setBlockNow(parseInt(pageNow/blockSize));
@@ -120,71 +95,6 @@ const MerchantList=()=> {
         <div className="container" id={"merchan_list"}>
             <h2 className="mt-4">리스트로 찾아보기</h2>
             <Table striped bordered hover className="list_table">
-                {/*<thead>*/}
-                {/*<tr>*/}
-                {/*    <th></th>*/}
-                {/*    <th></th>*/}
-                {/*    <th/>*/}
-                {/*    <th>*/}
-                {/*        <Dropdown isOpen={drop1Show} toggle={toggle1}>*/}
-                {/*            <DropdownToggle>*/}
-                {/*                {state} <select/>*/}
-                {/*            </DropdownToggle>*/}
-                {/*            <DropdownMenu*/}
-                {/*                modifiers={{*/}
-                {/*                    setMaxHeight: {*/}
-                {/*                        enabled: true,*/}
-                {/*                        order: 890,*/}
-                {/*                        fn: (data) => {*/}
-                {/*                            return {*/}
-                {/*                                ...data,*/}
-                {/*                                styles: {*/}
-                {/*                                    ...data.styles,*/}
-                {/*                                    overflow: 'auto',*/}
-                {/*                                    maxHeight: '100px',*/}
-                {/*                                },*/}
-                {/*                            };*/}
-                {/*                        },*/}
-                {/*                    },*/}
-                {/*                }}*/}
-                {/*            >*/}
-                {/*                {stateList.map((state)=>(*/}
-                {/*                    <DropdownItem onClick={()=>{stateCheck(state)}}>{state}</DropdownItem>))}*/}
-
-                {/*            </DropdownMenu>*/}
-                {/*        </Dropdown>*/}
-                {/*    </th>*/}
-                {/*    <th>*/}
-                {/*            <Dropdown isOpen={drop2Show} toggle={toggle2}>*/}
-                {/*                <DropdownToggle>*/}
-                {/*                    {cate} <select/>*/}
-                {/*                </DropdownToggle>*/}
-                {/*                <DropdownMenu*/}
-                {/*                    modifiers={{*/}
-                {/*                        setMaxHeight: {*/}
-                {/*                            enabled: true,*/}
-                {/*                            order: 890,*/}
-                {/*                            fn: (data) => {*/}
-                {/*                                return {*/}
-                {/*                                    ...data,*/}
-                {/*                                    styles: {*/}
-                {/*                                        ...data.styles,*/}
-                {/*                                        overflow: 'auto',*/}
-                {/*                                        maxHeight: '100px',*/}
-                {/*                                    },*/}
-                {/*                                };*/}
-                {/*                            },*/}
-                {/*                        },*/}
-                {/*                    }}*/}
-                {/*                >*/}
-                {/*                    {cateList.map((category)=>(<DropdownItem onClick={()=>cateCheck(category)}>{category}</DropdownItem>))}*/}
-                {/*                </DropdownMenu>*/}
-                {/*            </Dropdown>*/}
-                {/*        </th>*/}
-                {/*    <th/>*/}
-                {/*</tr>*/}
-                {/*</thead>*/}
-
                 <tbody>
                 <tr>
                     <td>#</td>
@@ -218,7 +128,6 @@ const MerchantList=()=> {
                             {pageNumArr.map((num)=>
                                 (
                                 <td className={"nums"} onClick={()=>setPageNow(num)}>&nbsp;{num+1}</td>
-                                   // <td style={{color:"red"}} onClick={()=>setPageNow(num)}>&nbsp;{num+1}</td>
                                 )
                             )}
 
